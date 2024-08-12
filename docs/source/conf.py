0d9827bec5c4b7d3251628c410b337ff612928a1
Leon <33361643+xleonplayz@users.noreply.github.com>
2024-08-12 14:48:15 +0000

    cd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
 project = 'LLY-HDC'
 copyright = '2024, Leon Kaiser'
 author = 'Leon Kaiser'
 release = '3. August 2024'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -22,14 +14,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
 ]
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../'))
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
